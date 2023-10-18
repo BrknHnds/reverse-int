f392ef505d8471cc55cbea90bac089359d81394b
BrknHnds <bsbd42@yandex.ru>
2023-10-18 15:33:31 +0500

    feat: add algorithm 2023-10-18 Ср 15:33:31

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.sqrt(n * n); // модуль для работы с отрицательными числами
+    let nArr = `${n}`.split(''); // приведение к массиву, чтобы работать методами массивов
+    for (let i = 0, j = nArr.length - 1; i < nArr.length / 2; i++, j--) {
+      [nArr[i], nArr[j]] = [nArr[j], nArr[i]]; // перестановка элементов массива
+    }
+    return +nArr.join('');
 }
